589e0dc34adf76da4d332a56502680e9148824bc
Hadenix <vsegda_online_007@bk.ru>
2022-01-10 15:32:11 +0300

    xpeng: initial
    Signed-off-by: Hadenix <vsegda_online_007@bk.ru>

---
+++ omni_xpeng.mk
@@ -0,0 +1,25 @@
+#
+# Copyright (C) 2021 The Android Open Source Project
+# Copyright (C) 2021 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Inherit from xpeng device
+$(call inherit-product, device/motorola/xpeng/device.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := xpeng
+PRODUCT_NAME := omni_xpeng
+PRODUCT_BRAND := motorola
+PRODUCT_MODEL := XT2175-2
+PRODUCT_MANUFACTURER := motorola
